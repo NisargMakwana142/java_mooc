cdc05a73093e80994fceb222a9e3fe9f09250eb3
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-08-25 10:05:56 +0530

    Seconds in a day

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -7,6 +7,9 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int value = Integer.valueOf(scanner.nextLine());
+        System.out.println(value * 86400);
 
     }
 }
