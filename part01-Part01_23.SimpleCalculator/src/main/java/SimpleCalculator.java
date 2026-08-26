fae3df5aa7f734faf17f071e6354fcb73b51bb08
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-08-26 08:50:23 +0530

    completed

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,21 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+
+        int sum = first+second;
+        int sub = first-second;
+        int mul = first*second;
+        double div = (double) first/second;
+
+        System.out.println(first+" + "+second+" = "+sum);
+        System.out.println(first+" - "+second+" = "+sub);
+        System.out.println(first+" * "+second+" = "+mul);
+        System.out.println(first+" / "+second+" = "+div);
 
     }
 }
