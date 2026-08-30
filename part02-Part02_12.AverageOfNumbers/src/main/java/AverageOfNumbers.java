53eb348a0e73da48927edbb44141d0eb0b7461f4
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-08-30 20:58:05 +0530

    completed

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,23 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int numberOfNumbers = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            sum += number;
+            numberOfNumbers += 1;
+        }
+
+        float averageOfNumbers = (float) sum/numberOfNumbers;
+        System.out.println("Average of the numbers: "+averageOfNumbers);
+
     }
 }
