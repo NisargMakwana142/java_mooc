b517d37a0af7b4e74ccc0755323990bc21a46e42
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-08-30 21:08:32 +0530

    completed

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,32 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int numberOfPositiveNumber = 0;
+
+        int numberOfNumber = 0;
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number > 0) {
+                sum += number;
+                numberOfNumber += 1;
+                numberOfPositiveNumber += 1;
+            }
+        }
+
+        if (numberOfNumber == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            double average = (double) sum/numberOfPositiveNumber;
+            System.out.println(average);
+        }
+
+
     }
 }
