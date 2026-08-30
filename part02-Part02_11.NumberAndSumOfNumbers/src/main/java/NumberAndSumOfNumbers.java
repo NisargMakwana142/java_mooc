f6701dc79bba707183a2f683198f5e41b3cd7a56
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-08-30 20:39:26 +0530

    completed

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -6,5 +6,23 @@ public class NumberAndSumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int numberOfNumbers = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                break;
+            }
+
+            sum += num;
+            numberOfNumbers += 1;
+        }
+
+        System.out.println("Number of numbers: "+numberOfNumbers);
+        System.out.println("Sum of the numbers: "+sum);
+
     }
 }
