66a88e767f08408255dcbad5021d4a090ea201b0
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-09-05 08:47:11 +0530

    completed

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -14,5 +14,43 @@ public class RepeatingBreakingAndRemembering {
         
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int numberOfNumbers = 0;
+        double average;
+        int even = 0;
+        int odd = 0;
+        
+
+        System.out.println("Give numbers:");
+        
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+
+            if (number % 2 == 0) {
+                sum += number;
+                numberOfNumbers++;
+                even++;
+            } else {
+                sum += number;
+                numberOfNumbers++;
+                odd++;
+            }
+
+        }
+
+        average = (double) sum/numberOfNumbers;
+
+        System.out.print("Sum: "+sum+"\n");
+        System.out.print("Numbers: "+numberOfNumbers+"\n");
+        System.out.print("Average: "+average+"\n");
+        System.out.print("Even: "+even+"\n");
+        System.out.print("Odd: "+odd);
+
     }
 }
