cd4cd594df1dd21bd5a9c3ff6b2f921c3cd46096
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-09-05 08:26:26 +0530

    completed

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,18 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a number: ");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        int factorial = 1;
+
+        while (number != 0) {
+            factorial *= number;
+            number--;
+
+        }
+
+        System.out.println("fact: "+factorial);
+
     }
 }
